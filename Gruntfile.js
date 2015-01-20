aa9091c12fc4b35c801e63b516f71d15abeb2af2
Joe Natalzia <jnatalzia@paperlesspost.com>
2015-01-20 18:45:54 -0500

    Set up state tracking for pane swiping

---
+++ Gruntfile.js
@@ -0,0 +1,107 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    haml: {
+      dist: {                            // Target
+        files: {                         // Dictionary of files
+          'public/index.html': 'src/index.haml'
+        }
+      },
+      partials: {
+        files: grunt.file.expandMapping(['src/partials/*.haml'], 'public/partials/', {
+          rename: function(base, path) {
+            return base + path.replace(/src\/partials/gi, '').replace(/\.haml$/, '.html');
+          }
+        })
+      }
+    },
+    compass: {                  // Task
+      dist: {                   // Target
+        options: {              // Target options
+          sassDir: 'src/styles',
+          cssDir: 'public/styles'
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          base: 'public',
+          keepalive: true,
+          hostname: "*",
+          open: {
+            target: '127.0.0.1:9001', // target url to open
+          },
+          livereload: 9000
+        }
+      }
+    },
+    copy: {
+      js: {
+        expand: true,
+        cwd: 'src/javascripts',
+        src: '**',
+        dest: 'public/javascripts',
+        flatten: false,
+        filter: 'isFile',
+      },
+      img: {
+        expand: true,
+        cwd: 'src/img',
+        src: '**',
+        dest: 'public/img',
+        flatten: false,
+        filter: 'isFile',
+      }
+    },
+    clean: {
+      public: ["public/"]
+    },
+    uglify: {
+      js: {
+        files: {
+          'public/javascripts/app.min.js': ['public/javascripts/app.js']
+        }
+      },
+    },
+    watch: {
+      options: {
+        livereload: {
+          port: 9000
+        }
+      },
+      scripts: {
+        files: ['src/javascripts/**/*.js'],
+        tasks: ['copy:js'],
+      },
+      images: {
+        files: ['src/img/**/*'],
+        tasks: ['copy:img']
+      },
+      scss: {
+        files: ['src/styles/**/*.scss'],
+        tasks: ['compass'],
+      },
+      haml: {
+        files: ["src/**/*.haml"],
+        tasks: ['haml']
+      }
+    }
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-haml');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['clean:public', 'haml', 'compass', 'copy', 'watch']);
+
+};
---
+++ src/javascripts/app.js
@@ -0,0 +1,117 @@
+$(function(){
+
+  var PP = {};
+  PP.CircleMenu = {
+    lastTransition: "",
+    currentHash: "",
+    currentIndex: 0,
+    states: [],
+    createHash: function() {
+      return Math.random().toString(36).substring(10);
+    },
+    saveState: function() {
+      var newHash = this.createHash();
+      this.states.push({
+        content: $('.js-viewport-current').html(),
+        hash: newHash,
+        scroll: $('.js-viewport-current').scrollTop()
+      });
+      this.currentHash = newHash;
+    },
+    saveStateScroll: function() {
+      this.states[this.currentIndex].scroll = $('.js-viewport-current').scrollTop();
+    }
+  };
+
+  PP.CircleMenu.createHash();
+  PP.CircleMenu.saveState();
+
+  var hammer = new Hammer(document.querySelector('.js-vpt-ctnr'), {});
+  var scaleTimeout;
+  $('.js-circle').on('click', function() {
+    $('.js-circle').toggleClass('rotate');
+    $('.js-menu-holder').toggleClass('open');
+  });
+
+  hammer.on('swipeleft', function() {
+    PP.CircleMenu.currentIndex++;
+    PP.CircleMenu.lastTransition = "left";
+    changeViewport();
+  });
+
+  hammer.on('swiperight', function() {
+    if (PP.CircleMenu.states.length > 1 && PP.CircleMenu.currentIndex > 0) {
+      PP.CircleMenu.currentIndex--;
+      PP.CircleMenu.lastTransition = "right";
+      changeViewport();
+    }
+  })
+
+  $('.js-slide-change').on('click', function() {
+    PP.CircleMenu.currentIndex++;
+    $('.js-circle').toggleClass('rotate');
+    $('.js-menu-holder').toggleClass('open');
+    PP.CircleMenu.lastTransition = "left";
+    changeViewport();
+  });
+
+  document.querySelector('.js-vpt-ctnr').addEventListener('webkitTransitionEnd', function() {
+    var direction = PP.CircleMenu.lastTransition;
+    var jsClass = PP.CircleMenu.lastTransition === "left" ? "js-viewport-next" : "js-viewport-prev";
+
+    $('.js-viewport-current').remove();
+    $('.' + jsClass).removeClass('js-viewport-next').addClass('js-viewport-current')
+
+    if (PP.CircleMenu.lastTransition === "left") {
+      $('.js-vpt-ctnr').append('<div class="viewport ' + jsClass + '"></div>').removeClass(direction);
+    } else {
+      $('.js-vpt-ctnr').prepend('<div class="viewport ' + jsClass + '"></div>').removeClass(direction);
+    }
+
+    if (!PP.CircleMenu.states[PP.CircleMenu.currentIndex]) {
+      PP.CircleMenu.saveState();
+    }
+
+    $('.js-viewport-current').on('scroll.saveState', function() {
+      PP.CircleMenu.saveStateScroll();
+    });
+    
+  });
+
+  $('.js-viewport-current').on('scroll.saveState', function() {
+    PP.CircleMenu.saveStateScroll();
+  });
+
+  function changeViewport() {
+    var jsClass = PP.CircleMenu.lastTransition === "left" ? "js-viewport-next" : "js-viewport-prev";
+
+    var appendInfo = function(r, s) {
+      $('.viewport').not('.js-viewport-current').scrollTop(0);
+      $('.js-viewport-current').off('scroll.saveState')
+
+      var newViewport = $('.'+jsClass);
+      if (PP.CircleMenu.lastTransition === "left") {
+        newViewport.append(r);
+      } else {
+        newViewport.prepend(r);
+      }
+      $('.js-vpt-ctnr').addClass(PP.CircleMenu.lastTransition);
+      if (s) {
+        newViewport.scrollTop(s);
+      }
+    }
+
+    if (PP.CircleMenu.states[PP.CircleMenu.currentIndex]) {
+      appendInfo(PP.CircleMenu.states[PP.CircleMenu.currentIndex].content, PP.CircleMenu.states[PP.CircleMenu.currentIndex].scroll);
+    } else {
+      $.ajax({
+        //replace me with your content
+        url: "partials/messages_panel.html",
+        type: "GET",
+        success: function(r) {
+          appendInfo(r);
+        }
+      });
+    }
+  }
+});
