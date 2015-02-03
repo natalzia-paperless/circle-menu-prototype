9ca65a0f5e23220640614ca12c0e4f272d7fd39e
Joe Natalzia <jnatalzia@paperlesspost.com>
2015-02-03 18:00:49 -0500

    Test performance
    Add in 3000 dom elements to each along with 2 different 1000 length
    arrays to test how the browser performs under stress.

--- src/javascripts/react_components/components.js
+++ src/javascripts/react_components/components.js
@@ -2,11 +2,44 @@ var ReactComponents = ReactComponents || {};
 
 ReactComponents.GuestList = React.createClass({
   getInitialState:function() {
+    var textArr = [];
+    var urls = [];
+    for (var i = 0; i < 1000; i++) {
+      var imgW = Math.floor(Math.random() * 100) + 300;
+      var imgH = Math.floor(Math.random() * 100) + 100;
+
+      var url = "http://insolitebuzz.fr/wp-content/uploads/2014/10/test-all-the-things.jpg";
+      textArr.push(Math.random().toString(36).substring(10));
+      urls.push(url);
+    }
+
     return {
-      text: Math.random().toString(36).substring(10)
+      text: textArr,
+      url: urls
     }
   },
+  changeOpacity: function(e) {
+
+  },
+  getAllComponents: function() {
+    var arr = [];
+    for (var i = 0; i < 100; i++) {
+      var txt = this.state.text[i];
+      arr.push(
+        <div>
+          <span>{txt}</span>
+          <h1>{txt}</h1>
+          <img data-id={i} onClick={this.changeOpacity} alt="" src={this.state.url[i]} />
+        </div>
+      );
+    }
+    return arr;
+  },
   render: function() {
-    return (<h1 className='text-el'>{this.state.text}</h1>);
+    return (
+      <div>
+        {this.getAllComponents()}
+      </div>
+    );
   }
 });
